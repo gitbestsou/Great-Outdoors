1fff2871d71be5d7b09042549e939b8d12c4c9ec
AnkushA055 <54537225+AnkushA055@users.noreply.github.com>
2019-09-15 14:04:36 +0530

    Add files via upload
    Order Class

---
+++ GreatOutdoorOrder/GreatOutdoorOrder.BusinessLayer/BL.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GreatOutdoorsOrder.DataAccessLayer;
+using GreatOutdoorsOrder.Entities;
+using GreatOutdoorsOrder.Exception;
+
+namespace GreatOutdoorsOrder.BusinessLayer
+{
+    public class Class1
+    {
+        private static bool ValidateOrder(Order order)
+        {
+            StringBuilder sb = new StringBuilder();
+            bool validOrder = true;
+            if (order.OrderID <= 0)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Invalid Guest ID");
+
+            }
+            if (order.RetailerID <= 0)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.SalesmanID <= 0)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.ProductID <= 0)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.Quantity <= 0)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.SellingPrice <= 0)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.TotalAmount <= 0)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.AmountPayable <= 0)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.FinalDelieveryAddress == string.Empty)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.ChannelOfSale == string.Empty)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.Status == string.Empty)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+            if (order.CategoryID <= 0)
+            {
+                validOrder = false;
+                sb.Append(Environment.NewLine + "Guest Name Required");
+
+            }
+
+
+            if (validOrder == false)
+                throw new OrderException(sb.ToString());
+            return validOrder;
+        }
+
+        public static bool AddOrderBL(Order newOrder)
+        {
+            bool orderAdded = false;
+            try
+            {
+                if (ValidateOrder(newOrder))
+                {
+                    OrderDAL guestDAL = new OrderDAL();
+                    orderAdded = guestDAL.AddOrderDAL(newOrder);
+                }
+            }
+            catch (OrderException)
+            {
+                throw;
+            }
+            catch (ApplicationException ex)
+            {
+                throw ex;
+            }
+
+            return orderAdded;
+        }
+
+        public static List<Order> GetAllOrdersBL()
+        {
+            List<Order> orderList = null;
+            try
+            {
+                OrderDAL orderDAL = new OrderDAL();
+                orderList = orderDAL.GetAllOrdersDAL();
+            }
+            catch (OrderException ex)
+            {
+                throw ex;
+            }
+            catch (ApplicationException ex)
+            {
+                throw ex;
+            }
+            return orderList;
+        }
+
+        public static Order SearcOrderBL(int searchOrderID)
+        {
+            Order searchOrder = null;
+            try
+            {
+                OrderDAL orderDAL = new OrderDAL();
+                searchOrder = orderDAL.SearchOrderDAL(searchOrderID);
+            }
+            catch (OrderException ex)
+            {
+                throw ex;
+            }
+            catch (ApplicationException ex)
+            {
+                throw ex;
+            }
+            return searchOrder;
+
+        }
+
+       
+
+        public static bool UpdateOrderBL(Order updateOrder)
+        {
+            bool orderUpdated = false;
+            try
+            {
+                if (ValidateOrder(updateOrder))
+                {
+                    OrderDAL orderDAL = new OrderDAL();
+                    orderUpdated = orderDAL.UpdateOrderDAL(updateOrder);
+                }
+            }
+            catch (OrderException)
+            {
+                throw;
+            }
+            catch (ApplicationException ex)
+            {
+                throw ex;
+            }
+
+            return orderUpdated;
+        }
+
+    }
+}
---
+++ GreatOutdoorOrder/GreatOutdoorOrder.Exceptions/Class1.cs
@@ -0,0 +1,27 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreatOutdoorsOrder.Exception
+{
+    public class OrderException : ApplicationException
+    {
+        public OrderException()
+            : base()
+        {
+        }
+
+        public OrderException(string message)
+            : base(message)
+        {
+        }
+        public OrderException(string message, ApplicationException innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
